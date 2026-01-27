d4ac6b2ce06898b128819fc47a1444d3464a5ce3
Rycarddo <joserycarddo2003@gmail.com>
2026-01-27 19:29:26 -0400

    .

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,6 +2,8 @@ import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { prisma } from "@/lib/prisma";
 
+const isProduction = process.env.NODE_ENV === "production";
+
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
     provider: "postgresql",
@@ -9,11 +11,13 @@ export const auth = betterAuth({
   baseURL: process.env.BETTER_AUTH_URL,
   secret: process.env.BETTER_AUTH_SECRET,
   trustedOrigins: [process.env.BETTER_AUTH_URL!],
+  advanced: {
+    useSecureCookies: isProduction,
+  },
   socialProviders: {
     google: {
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-      redirectURI: `${process.env.NEXT_PUBLIC_APP_URL}/api/auth/callback/google`,
     },
   },
 });
